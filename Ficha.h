691a01116761f936fe0d663e2c366e17f12866eb
wxmarcos <103674173+wxmarcos@users.noreply.github.com>
2022-06-04 02:11:21 -0300

    Update Ficha.h

--- Ficha.h
+++ Ficha.h
@@ -1,6 +1,10 @@
 #ifndef FICHA_H_
 #define FICHA_H_
 
+#ifndef NULL
+#define NULL 0
+#endif /* NULL */
+
 #include "Jugador.h"
 
 enum TipoFicha{
@@ -9,16 +13,23 @@ enum TipoFicha{
 	Barco
 };
 
+enum EstadoFicha{
+	Activa,
+	Eliminada
+};
+
 class Ficha {
 private:
+
 	TipoFicha tipo;
-	Jugador* jugador;
+	Jugador *jugador;
+	EstadoFicha estado;
 public:
 	/*
 	 * pre: el jugador no debe estar vacio
 	 * pos: Inicializa una ficha asignandole su tipo y el jugador dueño de la misma
 	 */
-	Ficha(TipoFicha tipo, Jugador* jugador);
+	Ficha(TipoFicha tipo, Jugador *jugador, EstadoFicha estado);
 	/*
 	 * pre:
 	 * pos: Devuelve el tipo de ficha
@@ -29,7 +40,21 @@ public:
 	 * pos: Devuelve el jugador dueño de la ficha
 	 */
 	Jugador* getJugador();
-
+	/*
+	 * pre:
+	 * pos: Devuelve el estado de la ficha
+	 */
+	EstadoFicha getEstado() const;
+	/*
+	 * pre:
+	 * pos: cambia el estado de la ficha
+	 */
+	void setEstado(EstadoFicha estado);
+	/*
+	 * pre: la ficha debe estar con estado activa
+	 * pos: Elimina la ficha
+	 */
+	void Eliminar();
 };
 
 #endif /* FICHA_H_ */
