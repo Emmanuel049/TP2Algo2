3154e1d280cc7559d8d24dda76ce6b8dd8ce9b38
wxmarcos <103674173+wxmarcos@users.noreply.github.com>
2022-06-04 02:10:57 -0300

    Update Ficha.cpp

--- Ficha.cpp
+++ Ficha.cpp
@@ -1,12 +1,13 @@
 #include "Ficha.h"
 #include "Jugador.h"
 
-Ficha::Ficha(TipoFicha tipo, Jugador* jugador){
+Ficha::Ficha(TipoFicha tipo, Jugador *jugador, EstadoFicha estado){
 	if (jugador == NULL){
 		throw "El jugador no puede estar vacio";
 	}
 	this->tipo=tipo;
 	this->jugador=jugador;
+	this->estado=estado;
 
 }
 
@@ -17,3 +18,19 @@ TipoFicha Ficha::getTipo(){
 Jugador* Ficha::getJugador(){
 	return this->jugador;
 }
+
+EstadoFicha Ficha::getEstado() const{
+	return estado;
+}
+
+void Ficha::setEstado(EstadoFicha estado){
+	this->estado=estado;
+}
+
+void Ficha::Eliminar(){
+	if (this->estado== Eliminada){
+		throw "La ficha ya esta eliminada";
+	}
+	this->estado=Eliminada;
+}
+
