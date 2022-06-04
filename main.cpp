64ef4ac2884cc7a31386cece9283d0cba18a592d
wxmarcos <103674173+wxmarcos@users.noreply.github.com>
2022-06-04 01:21:11 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 
 int main(){
 	Tablero * miTablero=new Tablero(2,5,5);
-	miTablero->getCasilla(2,3,2)->setFicha(Avion);
-	miTablero->getCasilla(1,3,2)->setFicha(Barco);
+	Jugador * jugador;
+	miTablero->getCasilla(2,3,2)->setFicha(Ficha(Avion, jugador));
+	miTablero->getCasilla(1,3,2)->setFicha(Ficha(Barco, jugador));
 	miTablero->printTablero();
 	delete miTablero;
 	return 0;
 }
+
